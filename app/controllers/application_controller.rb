e767af759cb0a46b341ff21519cec0fc2c18cf11
ghigbie <gehigbie@gmail.com>
2021-11-13 19:16:34 -0500

    Add routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def hello_doggie
+        render html: "Hello Doggie!"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get 'home', to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'contactus', to 'pages#contact'
+  # get 'contact' to: 'pages#contact'
 end
